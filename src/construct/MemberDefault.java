9b841ed56193431ea1aca012888a74db1832b929
Choi-Hyeok99 <gur0709@naver.com>
2024-06-27 14:16:23 +0900

    study : default construct

---
+++ src/construct/MemberDefault.java
@@ -0,0 +1,12 @@
+package construct;
+
+public class MemberDefault {
+
+    String name;
+
+        // 자동으로 기본 생성자를 만들어줌
+        // 생성자가 하나라도 있으면 기본 생성자는 제공되지 않는다.
+//    MemberDefault(){
+//
+//    }
+}
